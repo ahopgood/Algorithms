255a333c5dada0b04bb957dd6634b4b0450e44b2
Alexander Hopgood <alex.hopgood@gmail.com>
2016-07-04 21:12:38 +0100

    Added test data for Ordered, Random and Reverse dataset of 10 elements.
    Added results of ten iterations of 10, 100 and 1000 element datasets for the InsertionSort.

--- src/test/java/com/alexander/examples/design/sorting/InsertionSortTest.java
+++ src/test/java/com/alexander/examples/design/sorting/InsertionSortTest.java
@@ -1,9 +1,6 @@
 package com.alexander.examples.design.sorting;
 
-import org.apache.log4j.BasicConfigurator;
-import org.apache.log4j.ConsoleAppender;
-import org.apache.log4j.PatternLayout;
-import org.apache.log4j.Priority;
+import org.apache.log4j.*;
 import org.junit.BeforeClass;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -29,21 +26,24 @@ public class InsertionSortTest {
 
     @BeforeClass
     public static void setUpBefore() throws Exception {
-        ConsoleAppender console = new ConsoleAppender();
+        int limit = 100;
+//        ConsoleAppender console = new ConsoleAppender();
+        RollingFileAppender console = new RollingFileAppender();
         console.setThreshold(Priority.toPriority(Priority.INFO_INT));
         console.setName("console");
-        console.setTarget("System.out");
+//        console.setTarget("System.out");
+        console.setFile(limit+"-"+InsertionSortTest.class.getSimpleName()+"-output.log", true, true, 4096);
         console.setLayout(new PatternLayout("%r [%t] %p %c %x - %m%n"));
         console.activateOptions();
         BasicConfigurator.configure(console);
-
-        int limit = 10;
 
         expected = Data.readData(limit, new Data.OrderedDataGenerator());
         random = Data.readData(limit, new Data.RandomDataGenerator());
         ordered = Data.readData(limit, new Data.OrderedDataGenerator());
         reverse = Data.readData(limit, new Data.ReverseOrderedDataGenerator());
     }
+
+    private String name;
 
     @org.junit.Before
     public void setUp() throws Exception {
@@ -54,41 +54,48 @@ public class InsertionSortTest {
     public void tearDown() throws Exception {
         end = System.currentTimeMillis();
         long runningTime = end - start;
-        log.info("{}ms {}.{}s", runningTime, runningTime/1000, runningTime%1000);
+        log.info("{} {}ms {}.{}s", name, runningTime, runningTime/1000, runningTime%1000);
+        name = "";
     }
 
     @org.junit.Test
     public void testSort_givenOrderedList() throws Exception {
+        name = "Random";
         List<Integer> result = InsertionSort.sort(ordered);
         assertEquals(expected, result);
     }
 
     @org.junit.Test
     public void testSort_givenMixedUnorderedList() throws Exception {
+        name = "Unordered";
         List<Integer> result = InsertionSort.sort(random);
         assertEquals(expected, result);
     }
 
     @org.junit.Test
     public void testSort_givenReverseOrderedList() throws Exception {
+        name = "Reverse";
         List<Integer> result = InsertionSort.sort(reverse);
         assertEquals(expected, result);
     }
 
     @org.junit.Test
     public void testSort_givenNullList() throws Exception {
+        name = "Null";
         List<Integer> result = InsertionSort.sort(null);
         assertEquals(new LinkedList<Integer>(), result);
     }
 
     @org.junit.Test
     public void testSort_givenEmptyList() throws Exception {
+        name = "Empty";
         List<Integer> result = InsertionSort.sort(new LinkedList<Integer>());
         assertEquals(new LinkedList<Integer>(), result);
     }
 
     @org.junit.Test
     public void testSort_givenSingleElementList() throws Exception {
+        name = "Single";
         List<Integer> unordered = new LinkedList<Integer>();
         unordered.add(0);
 
